c87c48d7069cc16148a6a0f8ac1abac58827b570
Sahan Ediriweera <rsahanediriweera@gmail.com>
2023-08-19 19:21:17 +0530

    added carousel

--- Frontend/e-commerce-frontend/src/app/page.tsx
+++ Frontend/e-commerce-frontend/src/app/page.tsx
@@ -1,8 +1,13 @@
+import ProductCard from "@/components/ProductCard"
+import Carousel from "@/components/Carousel/Carousel"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        This is the main content
+        <ProductCard/>
+        <section>
+          <Carousel/>
+        </section>
     </main>
   )
 }
