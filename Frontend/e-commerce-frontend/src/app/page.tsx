9eea52edb02b81d32ed3576029d2d7ea44a95ef2
Sahan Ediriweera <107974885+sahanediriweera@users.noreply.github.com>
2023-08-20 11:13:58 +0530

    Merge pull request #4 from chxru/imporving-sidebar
    fixed carousels

--- Frontend/e-commerce-frontend/src/app/page.tsx
+++ Frontend/e-commerce-frontend/src/app/page.tsx
@@ -5,8 +5,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <ProductCard/>
-        <section>
+        <section className="ml-[16rem] pr-[8rem] mt-[-4rem] flex flex-col">
+          <div className="">
           <Carousel/>
+          <Carousel/>
+          </div>
         </section>
     </main>
   )
