9c5ce4ddce655487167810a10a3b33e4c32c8eff
Sahan Ediriweera <rsahanediriweera@gmail.com>
2023-08-20 11:13:12 +0530

    fixed carousels

--- Frontend/e-commerce-frontend/src/components/ProductCard.tsx
+++ Frontend/e-commerce-frontend/src/components/ProductCard.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import {Card, CardBody, CardFooter, Image,Button} from "@nextui-org/react";
 import list from '@/data/cardDetails';
 
-export default function App() {
+export default function ProductCard() {
 
 
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-20">
+    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-20 w-full mt-[-7rem]">
       {list.map((item, index) => (
         <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
           <CardBody className="overflow-visible p-0">
@@ -16,7 +16,7 @@ export default function App() {
               radius="lg"
               width="100%"
               alt={item.title}
-              className="w-full object-cover h-[140px]"
+              className="w-full object-cover h-[300px]"
               src={item.img}
             />
             <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
