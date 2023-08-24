82c615412f043207ce0f1f4a2e12933e4d6e3912
Charuka Samarakoon <charuka@protonmail.com>
2023-08-25 04:37:42 +0530

    fix(fe): :bug: incorrect store access, incorrect baseUrl

--- fe/util/axios.ts
+++ fe/util/axios.ts
@@ -2,9 +2,9 @@ import { useUserStore } from "@/store/user.store";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:4000",
   headers: {
-    Authorization: `Bearer ${useUserStore().access_token}`,
+    Authorization: `Bearer ${useUserStore.getState().access_token}`,
   },
 });
 
