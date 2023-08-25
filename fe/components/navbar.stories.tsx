5914de6fd148b9654634b8f1493be6fad476ea07
Charuka Samarakoon <charuka@protonmail.com>
2023-08-26 01:56:30 +0530

    feat(fe): :lipstick: add navbar and searchbar stories

---
+++ fe/components/navbar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Topbar from "./navbar.component";
+
+const meta = {
+  title: "Navigation/Topbar",
+  component: Topbar,
+  tags: ["autodocs"],
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+} satisfies Meta<typeof Topbar>;
+
+export default meta;
+type Story = StoryObj<typeof Topbar>;
+
+export const Default: Story = {};
