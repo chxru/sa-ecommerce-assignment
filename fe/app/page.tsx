b487a4bc42939ee8ba96c092776eaa87ee063ce6
Charuka Samarakoon <charuka@protonmail.com>
2023-09-06 05:06:52 +0530

    feat(fe): :poop: show paginated query data

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -1,19 +1,61 @@
 "use client";
 
-import { useUserStore } from "@/store/user.store";
-import { FunctionComponent } from "react";
+import { Fetcher } from "@/util/axios";
+import { PaginatedProductQueryResponse } from "@saecom/types";
+import { Pagination } from "flowbite-react";
+import { FunctionComponent, useEffect, useState } from "react";
+import useSWR from "swr";
+
+const LIMIT = 10;
 
 const HomePage: FunctionComponent = () => {
-  const store = useUserStore();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const { data: res, isLoading } = useSWR<PaginatedProductQueryResponse>(
+    `/products/page/${currentPage}/limit/${LIMIT}`,
+    Fetcher.get,
+    {
+      revalidateOnFocus: false,
+    }
+  );
+
+  useEffect(() => {
+    if (!res?.data.metadata[0].total) {
+      return;
+    }
+
+    const pages = Math.ceil((res?.data.metadata[0].total || LIMIT) / LIMIT);
+
+    if (totalPages !== pages) {
+      setTotalPages(pages);
+    }
+  }, [res, totalPages]);
 
   return (
     <main>
-      <div className="flex h-screen w-full items-center justify-center">
-        {store.user?.username ? (
-          <h1>Hello {store.user.username}</h1>
-        ) : (
-          <h1>SAECOM</h1>
-        )}
+      <div className="flex flex-col h-screen w-full  mt-28 ml-8">
+        <h1>Latest Arrivals</h1>
+
+        <div className="flex flex-wrap mt-4 flex-col">
+          {res?.data.data.map((product) => (
+            <div
+              key={product._id}
+              className="flex flex-col items-center justify-center w-1/4"
+            >
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </div>
+          ))}
+
+          <Pagination
+            currentPage={currentPage}
+            onPageChange={(page) => {
+              setCurrentPage(page);
+            }}
+            totalPages={totalPages}
+          />
+        </div>
       </div>
     </main>
   );
