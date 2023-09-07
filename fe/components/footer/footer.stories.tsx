fa9f5e36d674d383517271b3cc0bf2fb3b2378ca
wick98 <wickx98@gmail.comm>
2023-09-07 21:27:38 +0530

    Add frontend components: footer, card, checkout summary, contact us

---
+++ fe/components/footer/footer.stories.tsx
@@ -0,0 +1,14 @@
+// stories/Footer.stories.tsx
+import React from 'react';
+import Footer from './footer.component';
+
+export default {
+  title: 'Footer/Footer',
+  component: Footer,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const Default = () => <Footer />;
