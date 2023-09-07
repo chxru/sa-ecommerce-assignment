01dfd100342ed7f24e8297902c5b3ead31c5d42d
Sahan Ediriweera <rsahanediriweera@gmail.com>
2023-09-07 23:54:02 +0530

    started adding carousels for products page

---
+++ fe/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+import ProductCarousel from "@/components/carousel/product.carousel";
+
+const ProductPage : FunctionComponent = ()=>{
+    return(
+    <div>
+        <ProductCarousel/>
+    </div>)
+}
+
+export default ProductPage;
---
+++ fe/components/carousel/product.carousel.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const ProductCarousel: FunctionComponent = () => {
+    return(
+        <div className="bg-black">
+            This is the product carousel component
+        </div>
+    )
+}
+
+export default ProductCarousel;
