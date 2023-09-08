ac10d0175827ba7db8f0f537a65c42bf3aea3699
wick98 <wickx98@gmail.comm>
2023-09-08 22:10:36 +0530

    Implemented Shipping Details component

---
+++ fe/components/shippingdetails/shippingdetails.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import ShippingDetails from './shippingdetails.component';
+
+export default {
+  title: 'Products/ShippingDetails',
+  component: ShippingDetails,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+} as Meta;
+
+const Template: Story = (args) => <ShippingDetails {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
