feec3811d2dd8009f988d91f634b774debc73a7e
wick98 <wickx98@gmail.comm>
2023-09-09 13:25:01 +0530

    modify card component

--- fe/components/productcard/productcard.component.tsx
+++ fe/components/productcard/productcard.component.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from 'react'
+"use client";
+import React, { FunctionComponent, useState } from 'react'
 
 interface productProps {
   productImage : string;
@@ -6,7 +7,21 @@ interface productProps {
   price : number;
 }
 
-const productCard : FunctionComponent <productProps> = (props) => {
+const productCard: FunctionComponent<productProps> = (props) => {
+  const [isRed, setIsRed] = useState(false);
+
+  const toggleRed = () => {
+    setIsRed(!isRed);
+  };
+
+  const buttonClasses = `rounded-full w-10 h-10 p-0 border-0 inline-flex items-center justify-center ml-4 ${
+    isRed ? 'bg-red-500' : 'bg-gray-200'
+  }`;
+
+
+
+
+
   return (
     
 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -34,11 +49,19 @@ const productCard : FunctionComponent <productProps> = (props) => {
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
             </svg>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
-            <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+            <button className={buttonClasses} onClick={toggleRed}>
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className={`w-5 h-5 text-${isRed ? 'white' : 'gray-500'}`}
+                viewBox="0 0 24 24"
+              >
                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
               </svg>
             </button>
+
         </div>
         <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}</span>
