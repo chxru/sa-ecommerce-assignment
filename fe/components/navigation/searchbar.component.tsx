f084dd1c9ef0b0eb20c2351730ecb56ff7708b99
Charuka Samarakoon <charuka@protonmail.com>
2023-09-09 07:44:34 +0530

    feat(fe): :sparkles: search bar redirects to search page

--- fe/components/navigation/searchbar.component.tsx
+++ fe/components/navigation/searchbar.component.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { FunctionComponent } from "react";
+import { useRouter } from "next/navigation";
+import { FunctionComponent, useState } from "react";
 
 const SearchBar: FunctionComponent = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const router = useRouter();
+
   return (
     <div className="relative mr-4">
       <input
         type="text"
         className="border-b-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         placeholder="Search"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        // capture enter key
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            router.push(`/search?q=${searchQuery}`);
+          }
+        }}
       />
     </div>
   );
